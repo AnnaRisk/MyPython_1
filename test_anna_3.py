315e4132d5b7ffc3bdeaa8400d3e2f7e9bce9cd1
Анна <annarainchik@gmail.com>
2022-06-16 19:59:25 +0300

    change 29 + test1

--- test_anna_3.py
+++ test_anna_3.py
@@ -4,25 +4,13 @@ import pytest
 
 
 @pytest.fixture()
-def test_before_each1(request):
+def test_open_web11():
     browser.open('https://google.com')
-
-
-@pytest.fixture()
-def test_size_window1(before_each):
     browser.config.window_width = 1200
     browser.config.window_height = 600
 
-
-@pytest.fixture()
-def test_search_loc1(before_each):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
 
-
-def test_positiv_test1():
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
-@pytest.fixture()
-def test_negativ_test(before_each):
-    browser.element('[id="search"]').should(have.text('error'))
